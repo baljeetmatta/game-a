6692442253020c781b9ee301b04125699d320ca1
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-02-11 10:32:56 +0530

    11-02-2026

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@
 //4. Gravity
 //5. Movement
 //6. Restrict Screen
+//7. Platform
+//8. Multiple Platform
+//9. Scrolling Effect
+//10. Win Situation
+
+let offset=0;
+const speed=2;
+
 
 const gameCanvas=document.querySelector("#gameCanvas");
 gameCanvas.width=window.innerWidth;
@@ -65,7 +73,10 @@ class Player{
         if(this.position.y+this.height+this.velocity.y<gameCanvas.height)
              this.velocity.y+=gravity;   
         else
-            this.velocity.y=0;
+            {this.velocity.y=0;
+                window.location.reload();
+
+            }
 
  
 
@@ -74,32 +85,91 @@ this.draw();
 }
 const player=new Player();
 player.draw();
-const platform=new Platform(300,gameCanvas.height-75,30,75);
+const platforms=[];
+
+//const platform=new Platform(300,gameCanvas.height-75,30,75);
+const platform=new Platform(200,200,100,30);
+
+platforms.push(platform);
+
+//const platform1=new Platform(550,gameCanvas.height-75,30,75);
+const platform1=new Platform(500,300,130,25);
+const platform2=new Platform(0,gameCanvas.height-40,400,40);
+const platform3=new Platform(450,gameCanvas.height-40,400,40);
+
+platforms.push(platform1);
+platforms.push(platform2);
+platforms.push(platform3);
+
 platform.draw();
 
 function animate(){
     requestAnimationFrame(animate);
-    context.clearRect(0,0,window.innerWidth,window.innerHeight)
-   platform.draw();
+    context.clearRect(0,0,window.innerWidth,window.innerHeight);
+    backImage.draw();
+    hillsImage.draw();
+
+  platforms.forEach((platform)=>{
+        platform.draw();
+  })
+    
     player.update();
     
 
 
     if(keys.right.pressed && player.position.x<=600)
-        player.velocity.x=2;
+    {    player.velocity.x=speed;
+       // offset+=speed;
+    }
     else if (keys.left.pressed && player.position.x>100)
-        player.velocity.x=-2;
+       { player.velocity.x=-speed;
+   // offset-=speed;
+       }
     else
+    {
         player.velocity.x=0;
+        if(keys.right.pressed)
+            {offset+=speed;
+            platforms.forEach((item)=>{
+                item.position.x-=speed;
+                
+            })}
 
+            if(keys.left.pressed){
+                offset-=2;
 
-    if(player.position.x+player.width>=platform.position.x)
+            platforms.forEach((item)=>{
+                item.position.x+=speed;
+                
+            })
+        }
+
+    }
+
+    platforms.forEach((platform)=>{
+
+ if(player.position.x+player.width>=platform.position.x
+        && player.position.x+player.width<=platform.position.x+platform.width &&
+         player.position.y+player.height>=platform.position.y &&
+        player.position.y<=platform.position.y+platform.height)
             player.velocity.x=0;
+
+            if(player.position.y+player.height<=platform.position.y
+
+                && player.position.y+player.height+player.velocity.y>=platform.position.y
+                && player.position.x+player.width>=platform.position.x
+        && player.position.x<=platform.position.x+platform.width
+       
+
+            )
+                player.velocity.y=0;
+    })
+   
+    console.log(offset);
 
        
 }
 
-animate();
 
 addEventListener("keydown",(e)=>{
     console.log(e);
@@ -142,3 +212,26 @@ addEventListener("keyup",(e)=>{
 //         player.velocity.x=0;
 
 // })
+
+class GenericImage
+{
+    constructor(imagePath)
+    {
+        const image=new Image();
+        image.src=imagePath;
+        this.image=image;
+
+    }
+    draw()
+    {
+        context.drawImage(this.image,0-offset,0);
+
+    }
+}
+const backImage=new GenericImage("./images/background.png");
+
+const hillsImage=new GenericImage("./images/hills.png");
+
+
+
+animate();
